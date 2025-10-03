6a292dd87ad475f38de0334264867dbafc96c3e5
YutamasPRO <167945323+YutamasPRO@users.noreply.github.com>
2025-10-03 16:43:31 -0500

    Update Login.jsx camila

--- frontend/sky-gestor-frontend/src/views/Login.jsx
+++ frontend/sky-gestor-frontend/src/views/Login.jsx
@@ -1,3 +1,5 @@
+//modificacion3
+
 import React, { useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext.js'; // Asegúrate de la extensión .js
