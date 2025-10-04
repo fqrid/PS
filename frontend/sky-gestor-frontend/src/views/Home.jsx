66fabdca48a6a8c69bf55eedb05f34227b732bcd
YutamasPRO <167945323+YutamasPRO@users.noreply.github.com>
2025-10-03 19:36:53 -0500

    Update Home.jsxcamila

--- frontend/sky-gestor-frontend/src/views/Home.jsx
+++ frontend/sky-gestor-frontend/src/views/Home.jsx
@@ -3,7 +3,13 @@
 // src/views/Home.jsx
 import React from 'react';
 import '../styles/estilosEventos.css';
-
+function Home() {
+  return (
+    <div className="fondo-personalizado" style={{ backgroundImage: 'url("/imagen/IMG_5994.JPEG")' }}>
+      <div className="container mt-5 pt-5">
+        <h2 className="text-center mb-4">Bienvenido a Sky – Gestor de Horarios</h2>
+        <p className="lead text-center">
+          Desde aquí puedes gestionar tus tareas, eventos y usuarios.
 
         </p>
       </div>
